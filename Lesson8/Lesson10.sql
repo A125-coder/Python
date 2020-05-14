1c2a815c44d46f65d6d41b26c69bd4604b14bd7d
Anna Romanovska <sup2a1nn@gmail.com>
2020-05-14 20:02:43 +0300

    Start with Django

---
+++ Lesson8/Lesson10.sql
@@ -0,0 +1,6 @@
+-- Используя внутреннее объединение в результирующей таблице получите имена продавцов и соответствующие им имена их клиентов, суммы их продаж и город... подсказка: для получения результата необходимо производить двойное объединение - объединение продавцов с клиентами на основе поля snum и второе объединение заказов с клиентами на основе поля cnum... результат должен быть представлен одним запросом
+SELECT s.sname, c.cname, s.city, o.amt FROM salers s INNER JOIN customers c ON c.snum = s.snum INNER JOIN orders o ON c.cnum = o.cnum;
+
+SELECT s.sname, c.cname, o.amt, s.city FROM salers s
+    INNER JOIN customers c ON c.snum = s.snum
+    INNER JOIN orders o ON o.cnum = c.cnum;
